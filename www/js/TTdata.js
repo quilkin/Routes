3674b63aac15906ecc1713e0d487c8c10e363c15
quilkin <quilkin2@gmail.com>
2020-10-15 11:18:58 +0100

    convert to iis express

--- www/js/TTdata.js
+++ www/js/TTdata.js
@@ -60,7 +60,7 @@ var bleData = (function ($) {
                 //return "http://192.168.1.73:54684/Service1.svc/";
             }
            //return "/Routes.svc/";
-           return "http://localhost/routes/Routes.svc/";
+            return "http://localhost:54684/Routes.svc/";
 
         },
         webRequestFailed = function (handle, status, error) {
